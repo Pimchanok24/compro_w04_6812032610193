f1113a7cc61b4450ce096c7eb04b814bd8f0eda4
Pimchanok24 <s6812032610193@email.kmutnb.ac.th>
2025-07-18 16:56:21 +0700

    First Time

---
+++ coding-w04-03.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+int main() {
+    char nameInitial = 'P';       
+    int age = 19;                 
+    float weight = 103.5;          
+    char gender[] = "Female";     
+    printf("= %c nameInitial\n", nameInitial); 
+    printf("age = %d\n", age);                  
+    printf("weight = %.2f\n", weight);          
+    printf("gender = %s\n", gender);           
+    return 0;  
+}
